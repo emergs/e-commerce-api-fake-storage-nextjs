486747d44484a86492420193629db6c5618e844b
Emerson Gonçalves dos Santos <emer_gs@hotmail.com>
2023-04-12 04:53:48 +0000

    feat:busca dos produtos na api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { log } from 'console'
 
 interface IProducts {
   category: string,
@@ -16,14 +17,17 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-  const [products, setProducts] = useState<IProducts>([])
+  const [products, setProducts] = useState<IProducts[]>([])
 
-  fetch('https://fakestoreapi.com/products')
-    .then(res => res.json())
-    .then(json => setProducts(json))
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(json => setProducts(json))
+  }, [])
+
 
   return (
-    <main className="flex min-h-screen bg-orange-700 text-xl">
+    <main className="flex flex-col min-h-screen bg-orange-700 text-xl">
       <div className='flex flex-row justify-between h-20 w-screen bg-orange-800 grid grid-cols-2 gap-4'>
         <div className='h-full flex'>
           <h1 className='self-center'>Fake Storage</h1>
@@ -36,11 +40,11 @@ export default function Home() {
           <li>lll</li>
         </ul>
       </div>
-      <div>
+      <div className='h-full w-full'>
         <ul>
           {
-            products?.map((elem: any) => {
-              <li>{elem.image}</li>
+            products.map((elem) => {
+              return <li><img src={elem.image} alt={elem.title} /></li>
             })
           }
         </ul>
