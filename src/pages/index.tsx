ed8810a95921ab56fa9bd3b9ba0b63170226f4ef
Emerson Gonçalves dos Santos <emer_gs@hotmail.com>
2023-04-13 04:49:17 +0000

    feat:insserção do title, price e description das imagens

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { Inter } from 'next/font/google'
 import { useEffect, useState } from 'react'
-import { log } from 'console'
+
 
 interface IProducts {
   category: string,
@@ -25,26 +26,36 @@ export default function Home() {
       .then(json => setProducts(json))
   }, [])
 
+  console.log(products)
 
   return (
     <main className="flex flex-col min-h-screen bg-orange-700 text-xl">
+      <div className='flex justify-end'>
+        <span><Link href="/register">Cadastre-se</Link></span>
+        <span><Link href="/login">Entrar</Link></span>
+      </div>
       <div className='flex flex-row justify-between h-20 w-screen bg-orange-800 grid grid-cols-2 gap-4'>
         <div className='h-full flex'>
           <h1 className='self-center'>Fake Storage</h1>
         </div>
         <ul className='flex flex-row justify-evenly self-center'>
-          <li>dddd</li>
-          <li>ssss</li>
-          <li>aaaa</li>
-          <li>jjj</li>
-          <li>lll</li>
+          <li>home</li>
+          <li>promoções</li>
+          <li>contato</li>
+          <li>carreira</li>
+          <li>sobre</li>
         </ul>
       </div>
       <div className='h-full w-full'>
-        <ul>
+        <ul className='flex flex-wrap justify-center'>
           {
             products.map((elem) => {
-              return <li><img src={elem.image} alt={elem.title} /></li>
+              return <li className='w-96 h-auto p-5 flex flex-col'>
+                <img className='h-50  w-full' src={elem.image} alt={elem.title} />
+                <span>{elem.title}</span>
+                <span>{`R$${elem.price}`}</span>
+                <p>{elem.description}</p>
+              </li>
             })
           }
         </ul>
